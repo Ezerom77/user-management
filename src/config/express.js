e227d205cedcbef0b3582189222db29ced59a156
Ezerom77 <fullstack@ezerom.com.ar>
2023-03-17 20:08:03 -0300

    Http and Mongo Configured

---
+++ src/config/express.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const expressApp = express();
+
+// TODO: agregar middlewares, routes etc
+
+export default expressApp
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import { printHello } from '#Lib/hello.js';
+import connectDB from '#config/db.js';
+import '#config/env.js';
+import httpServer from '#config/http.js';
 
-printHello();
+const bootstrap = async () => {
+  await connectDB(process.env.MONGODB_URL)
+  httpServer.listen(process.env.PORT, () => {
+    console.log("Server listening on port " + process.env.PORT)
+  })
+};
+bootstrap();
